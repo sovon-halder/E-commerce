5b0a21c44ee4ea887fa29350c57eca8363643f92
socon <sovon.cse8.nu@gmail.com>
2024-12-07 15:36:33 +0600

    home page

--- src/components/static/Home.jsx
+++ src/components/static/Home.jsx
@@ -1,30 +1,22 @@
 export default function Home() {
     return (
-     
-        <div id="ful-home-con" className="flex w-full flex-col lg:flex-row">
-        <div className="card bg-base-300 rounded-box grid h-40      flex-grow place-items-center" id="photo-con">
-
-        <div class="home-photo"> <img alt="Person's image" src="../assets/home.jpg"/></div>
-       
-
-
-        </div>
-
-        <div className="divider lg:divider-horizontal"></div>
-        <div className="card bg-base-300 rounded-box grid h-40          flex-grow place-items-center">
-
-        <div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, iure dignissimos ut nobis voluptatum officiis ipsam aspernatur quidem. Delectus nisi iste id nesciunt dolorum maxime cumque atque eaque cupiditate dolorem!
-            Cumque laboriosam nulla architecto molestiae deserunt. Possimus cupiditate dolores placeat fugit eius doloribus esse. Doloribus nostrum dolorum nam adipisci error animi mollitia minus, hic expedita? Placeat ab quis expedita iste?
-            Nesciunt facere inventore molestiae corporis culpa animi impedit quia odit odio quaerat quo commodi dolor sit harum voluptatem, eius voluptas. Odit ex repellat iusto, ipsa nihil consequuntur aspernatur maxime voluptatum.
-            nobis except
-            Eos incidunt vero recusandae autem dolore, facere velit saepe placeat vel itaque eaque, accusamus dolores quas beatae fugiat assumenda voluptatum! Reprehenderit totam nisi omnis ex nesciunt ipsa quos blanditiis accusantium!
-            Quaerat aut, ducimus distinctio temporibus amet culpa, alias quo laboriosam provident consectetur accusamus deleniti illo fuga est sit? Nisi, reiciendis! Adipisci, temporibus nam dolorem reiciendis quaerat illum assumenda exercitationem sequi.</p>
-        </div>
-
-        </div>
-        </div>
         
+  <div className="flex w-full flex-col lg:flex-row">
+  <div className="card bg-base-300 rounded-box grid h-32 flex-grow place-items-center">
+  <div class="home-photo"> <img alt="image" src="../assets/home.jpg"/></div>
+  </div>
+
+  <div className="divider lg:divider-horizontal"></div>
+
+  <div className="card bg-base-300 rounded-box grid h-32 flex-grow place-items-center">
+  <div class="home-photo"> <img alt="image" src="../assets/home.jpg"/></div>
+  </div>
+</div>
+
+
+
+
+
         
 
     );
