42751025ec02418c045ffeb2541a536a7f30f310
socon <sovon.cse8.nu@gmail.com>
2024-12-09 01:46:47 +0600

    home page design

--- src/components/static/Home.jsx
+++ src/components/static/Home.jsx
@@ -10,9 +10,16 @@ export default function Home() {
 
 
         <div class="right-col column">
-          <div>
-            <h1> hello </h1>
+          <div class="home-heading">
+            <h6> hello </h6>
           </div>
+          
+          <div class="para-heading">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor nulla veritatis quaerat, odit ipsa exercitationem repellat laudantium perferendis illo ea cum consequuntur quas corporis adipisci iure totam! Velit, ullam numquam?
+          Nobis, perspiciatis expedita eveniet ex nisi minima, eum reiciendis suscipit ipsam explicabo repellendus praesentium voluptatem soluta voluptates reprehenderit corporis in vitae? Architecto assumenda corporis error corrupti iusto enim dignissimos accusamus.
+          Quam repellendus culpa dolore, ipsam blanditiis molestias est et vel. Maiores enim rem suscipit eveniet quod error qui molestiae ducimus, ipsa illum ullam. Officiis optio amet mollitia aut deserunt enim.</p>
+          </div>
+          
         </div>
 
       </div>
