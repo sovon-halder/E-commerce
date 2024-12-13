f0d9dd1f976185a6d56a49b4102184136b010fa2
socon <sovon.cse8.nu@gmail.com>
2024-12-14 00:05:43 +0600

    card section

--- src/components/static/Product.jsx
+++ src/components/static/Product.jsx
@@ -35,7 +35,39 @@ export default function Product() {
   </div>
 </div>
 
+<div className="card bg-base-100 w-96 shadow-xl">
+  <figure>
+    <img src="../assets/macbook pro.jpg" alt="macbook pro m1" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">
+    MacBook Air 13” and 15”
+      <div className="badge badge-secondary">2022</div>
+    </h2>
+    <p>M2 or M3 chip</p>
+    <div className="card-actions justify-end">
+      <div className="badge badge-outline">Learn more</div>
+      <div className="badge badge-outline">Buy</div>
+    </div>
+  </div>
+</div>
 
+<div className="card bg-base-100 w-96 shadow-xl">
+  <figure>
+    <img src="../assets/macbook pro.jpg" alt="macbook pro m1" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">
+    MacBook Air 13” and 15”
+      <div className="badge badge-secondary">2022</div>
+    </h2>
+    <p>M2 or M3 chip</p>
+    <div className="card-actions justify-end">
+      <div className="badge badge-outline">Learn more</div>
+      <div className="badge badge-outline">Buy</div>
+    </div>
+  </div>
+</div>
 
 
         </div>
