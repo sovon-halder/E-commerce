3c9cbc89e3547938ff0afb19ee26dce66b39c2f9
socon <sovon.cse8.nu@gmail.com>
2024-12-14 23:33:19 +0600

    about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import {  RouterProvider } from 'react-router-dom'
+import{AiOutlineSearch} from 'react-icond/ai'
 import './App.css'
+import'./About.css'
 import router from './routes/Routes'
+
 
 function App() {
 
--- src/components/static/About.jsx
+++ src/components/static/About.jsx
@@ -1,7 +1,16 @@
 export default function About() {
     return (
         <div>
-            about page
+            
+            <form className='w-[500px] relative'>
+                <div className='relative'>
+                    <input type="search" placeholder='Type Here' className='w-full p-4 rounded-full bg-slate-800' />
+                    <button/>
+                    <AiOutlineSearch/>
+                </div>
+            </form>
+
+
         </div>
     );
 }
