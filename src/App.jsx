4433bd8312e9dd7a56f11f3725494e2ac76f1a55
socon <sovon.cse8.nu@gmail.com>
2025-01-03 21:53:01 +0600

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import {  RouterProvider } from 'react-router-dom'
-import{AiOutlineSearch} from 'react-icond/ai'
+//import{AiOutlineSearch} from 'react-icond/ai'{not working}
 import './App.css'
 import'./About.css'
 import router from './routes/Routes'
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
-import MainLayout from "../layout/MainLayout"
-import Home from "../components/static/Home.jsx"
-import Blog from "../components/static/Blog.jsx"
-import Product from "../components/static/Product.jsx"
-import About from "../components/static/About.jsx"
-
+import MainLayout from "../layout/MainLayout";
+import Home from "../components/static/Home.jsx";
+import Blog from "../components/static/Blog.jsx";
+import Product from "../components/static/Product.jsx";
+import About from "../components/static/About.jsx";
+import login from "../login-Signup/login.jsx";
 
 
 const router = createBrowserRouter([
@@ -28,7 +28,15 @@ const router = createBrowserRouter([
             path:"/about",
              element:<About />,
          },
-
+         {
+            path:"/login",
+            element:<login/>,
+         },
+         {
+          path:"/sign-up",
+          element:<sign-up/>,
+         },
+         
 
       ]
     },
