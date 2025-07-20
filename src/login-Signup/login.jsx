6f24f476150d56c6aa77f9cfd83adc4fc34f2565
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-07-21 02:52:10 +0600

    form

--- src/login-Signup/login.jsx
+++ src/login-Signup/login.jsx
@@ -1,3 +1,5 @@
+import { FaUser,FaLock  } from "react-icons/fa";
+
 
 export default function Login(){
     return(
@@ -8,14 +10,24 @@ export default function Login(){
                     <form action="#">
                         <h1>Login</h1>
 
-                        <div className="textbox"></div>
-                            <input type="text" placeholder="Username" />
-
-                        <div className="textbox">
-                            <input type="password" placeholder="Password"/>
+                        <div className="inp-box">
+                            <input type="text" placeholder="Username" required/>
+                            <FaUser />
                         </div>
 
-                        <button className="btn">Login</button>
+                        <div className="inp-tbox">
+                            <input type="password" placeholder="Password" required/>
+                            <FaLock />
+                        </div>
+
+                        <div className="remember-forget">
+                            <input type="checkbox" id="remember-me"/>
+
+                            <label htmlFor="remember-me">Remember me</label>
+                            <a href="#">Forgot password?</a>
+                        </div>
+
+                        <button type="submit" className="btn">Login</button>
                         <p className="signup-link">Don't have an account? <a href="/signup">Sign up</a></p>
 
                     </form>
