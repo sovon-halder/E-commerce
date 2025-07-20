4be81aacf99443aefef3eb39eabb0a0b9b472996
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-07-21 02:29:41 +0600

    from

--- src/login-Signup/login.jsx
+++ src/login-Signup/login.jsx
@@ -1,7 +1,27 @@
 
 export default function Login(){
     return(
-        <p>THIS IS SOLVED</p>
+        <div>
+            <div className="wrapper">
+
+                <div className="form-box-login">
+                    <form action="#">
+                        <h1>Login</h1>
+
+                        <div className="textbox"></div>
+                            <input type="text" placeholder="Username" />
+
+                        <div className="textbox">
+                            <input type="password" placeholder="Password"/>
+                        </div>
+
+                        <button className="btn">Login</button>
+                        <p className="signup-link">Don't have an account? <a href="/signup">Sign up</a></p>
+
+                    </form>
+                </div>
+            </div>
+        </div>
 
     )
 }
