1687e8d06697ecaf2c4a49c321c4e7e57cfa16bc
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-07-21 17:23:45 +0600

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import {  RouterProvider } from 'react-router-dom'
 //import{AiOutlineSearch} from 'react-icond/ai'{not working}
 import './App.css'
 import'./About.css'
-import './login-Signup/login.css'
 import router from './routes/Routes'
 
 
