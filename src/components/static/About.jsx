48decbbcf21ca9a281caae201ab503728c5d1cfc
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-07-28 11:01:04 +0600

    section design

--- src/components/static/About.jsx
+++ src/components/static/About.jsx
@@ -52,20 +52,11 @@ export default function About() {
         <div class="absolute inset-px rounded-lg bg-white max-lg:rounded-b-4xl lg:rounded-r-4xl"></div>
         <div class="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
           <div class="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
-            <p class="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">Powerful APIs</p>
+            <p class="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">Another section</p>
             <p class="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget sem sodales gravida.</p>
           </div>
           <div class="relative min-h-120 w-full grow">
-            <div class="absolute top-10 right-0 bottom-0 left-10 overflow-hidden rounded-tl-xl bg-gray-900 shadow-2xl outline outline-white/10">
-              <div class="flex bg-gray-900 outline outline-white/5">
-                <div class="-mb-px flex text-sm/6 font-medium text-gray-400">
-                  <div class="border-r border-b border-r-white/10 border-b-white/20 bg-white/5 px-4 py-2 text-white">NotificationSetting.jsx</div>
-                  <div class="border-r border-gray-600/10 px-4 py-2">App.jsx</div>
-                </div>
-              </div>
-              <div class="px-6 pt-6 pb-14">
-              </div>
-            </div>
+            
           </div>
         </div>
         <div class="pointer-events-none absolute inset-px rounded-lg shadow-sm outline outline-black/5 max-lg:rounded-b-4xl lg:rounded-r-4xl"></div>
