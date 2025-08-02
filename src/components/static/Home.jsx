03979f7da18f9974643be56bd01579bb43efc66f
Sovon Halder <sovon.cse8.nu@gmail.com>
2025-08-02 15:25:20 +0600

    your meaningful commit message

--- src/components/static/Home.jsx
+++ src/components/static/Home.jsx
@@ -83,11 +83,11 @@ export default function Home() {
 
 <div>
 
-
+{/* Section with two cards and a divider */}
 
   <section>
  <div className="flex w-full">
-  <div className="card bg-base-300 rounded-box grid h-20 grow place-items-center">content</div>
+  <div className="card bg-primary-content rounded-box grid h-20 grow place-items-center">content</div>
   <div className="divider divider-horizontal">OR</div>
   <div className="card bg-base-300 rounded-box grid h-20 grow place-items-center">content</div>
 </div>
